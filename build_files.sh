1f1ee79fd9a66e9ce6d5186ab2334a8cc9070970
unknown <areeshashamsi11@gmail.com>
2024-08-14 21:38:56 -0700

    Fix build script

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,14 @@
-echo"BUILD START"
-python 3.12.1 -m pip install -r requirements.txt
-python 3.12.1 manage.py collectstatic --noinput --clear
-echo"BUILD END"
+#!/bin/bash
+
+echo "BUILD START"
+
+# Install dependencies
+pip3 install -r requirements.txt
+
+# Run migrations
+python3 manage.py migrate
+
+# Collect static files
+python3 manage.py collectstatic --noinput
+
+echo "BUILD END"
